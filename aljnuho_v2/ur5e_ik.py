65a7e9db22fd9f91015f0f88b6bc8bd0cb7d1baf
yuth <yuth@todo.com>
2026-04-16 18:05:26 +0900

    uu

--- aljnuho_v2/ur5e_ik.py
+++ aljnuho_v2/ur5e_ik.py
@@ -4,10 +4,47 @@ from pytransform3d.plot_utils import make_3d_axis
 from pytransform3d.transform_manager import TransformManager
 from pytransform3d.transformations import plot_transform
 import matplotlib.pyplot as plt
-from system_test import RobotController
-
+import rtde_control
+import rtde_receive
+import rtde_io
+import robotiq_gripper
+import time
 
 np.set_printoptions(precision=4, suppress=True, linewidth=200)
+
+
+class RobotController:
+
+    def __init__(self):
+        self.hostip = "192.168.0.39"
+        self.toolioport = 54321
+        self.rtde_frequency = 500.0
+        self.rtde_c = rtde_control.RTDEControlInterface(self.hostip)
+        self.rtde_r = rtde_receive.RTDEReceiveInterface(self.hostip)
+        self.rtde_i = rtde_io.RTDEIOInterface(self.hostip)
+
+        self.gripper = robotiq_gripper.RobotiqGripper()
+        self.gripper.connect(self.hostip, 63352)
+
+    def activate_gripper(self):
+        if self.gripper.is_active():
+            print("Gripper is active")
+        else:
+            self.gripper.activate()
+
+    def get_actual_q(self):
+        return self.rtde_r.getActualQ()
+
+    def get_jacobian(self):
+        return self.rtde_c.getJacobian()
+
+    def get_joint_torques(self):
+        return self.rtde_c.getJointTorques()
+
+    def move_gripper(self, position, speed=255, force=255):
+        ack = self.gripper.move(position, speed, force)
+        print(f"Move command ack: {ack}")
+        return ack
 
 
 class RobotUR5eKin:
@@ -16,7 +53,6 @@ class RobotUR5eKin:
         self.d = np.array([0.1625, 0, 0, 0.1333, 0.0997, 0.0996])
         self.alpha = np.array([np.pi / 2, 0, 0, np.pi / 2, -np.pi / 2, 0])
         self.a = np.array([0, -0.425, -0.3922, 0, 0, 0])
-
         self.bot = DhRobot(self.alpha, self.a, self.d)
 
     def solve_fk(self, q):
@@ -80,6 +116,11 @@ class RobotUR5eKin:
         origins = np.array([T_0i[:3, 3] for T_0i in world_tf])
         ax.plot(origins[:, 0], origins[:, 1], origins[:, 2], "k-o", linewidth=2)
 
+        # Draw plane at 0,0,0 for reference
+        plane_size = 0.5
+        ax.plot([-plane_size, plane_size], [0, 0], [0, 0], "r--")
+        ax.plot([0, 0], [-plane_size, plane_size], [0, 0], "g--")
+
         reach = np.sum(np.abs(self.a)) + np.sum(np.abs(self.d))
         lim = max(0.8, 1.1 * reach)
         ax.set_xlim([-lim, lim])
@@ -101,88 +142,9 @@ class RobotUR5eKin:
 
         return relative_tf, world_tf
 
-    def setup_live_plot(self, frame_size=0.08):
-        """Initialize live plotting environment. Call once before sending live q updates."""
-        self.ax = make_3d_axis(ax_s=1.0)
-        self.fig = self.ax.figure
-        plt.ion()  # Enable interactive mode
-
-        reach = np.sum(np.abs(self.a)) + np.sum(np.abs(self.d))
-        lim = max(0.8, 1.1 * reach)
-        self.ax.set_xlim([-lim, lim])
-        self.ax.set_ylim([-lim, lim])
-        self.ax.set_zlim([0.0, lim])
-        self.ax.set_xlabel("X [m]")
-        self.ax.set_ylabel("Y [m]")
-        self.ax.set_zlabel("Z [m]")
-        self.ax.set_box_aspect([1, 1, 1])
-
-    def plot_link_transforms_live(self, q, frame_size=0.08):
-        """Update live plot with new joint angles q. Call setup_live_plot() once first."""
-        if not hasattr(self, 'ax'):
-            self.setup_live_plot(frame_size)
-
-        # Clear previous plot
-        self.ax.clear()
-
-        # Recompute DH chain
-        relative_tf, world_tf = self.get_dh_chain(q)
-
-        # Plot frame axes
-        for i, T_0i in enumerate(world_tf):
-            plot_transform(ax=self.ax, A2B=T_0i, s=frame_size, name=f"L{i}")
-
-        # Draw link-centerline segments
-        origins = np.array([T_0i[:3, 3] for T_0i in world_tf])
-        self.ax.plot(origins[:, 0], origins[:, 1], origins[:, 2], "k-o", linewidth=2)
-
-        # Reapply axis properties
-        reach = np.sum(np.abs(self.a)) + np.sum(np.abs(self.d))
-        lim = max(0.8, 1.1 * reach)
-        self.ax.set_xlim([-lim, lim])
-        self.ax.set_ylim([-lim, lim])
-        self.ax.set_zlim([0.0, lim])
-        self.ax.set_xlabel("X [m]")
-        self.ax.set_ylabel("Y [m]")
-        self.ax.set_zlabel("Z [m]")
-        self.ax.set_title(f"UR5e Live: q = {np.round(q, 3)}")
-        self.ax.set_box_aspect([1, 1, 1])
-
-        # Update display without blocking
-        plt.pause(0.001)
-
-        return relative_tf, world_tf
-
-    # @staticmethod
-    # def _convert_urdf_to_dh_frame(H):
-    #     "from our design task in urdf frame to dh frame"
-    #     Hdh_to_urdf = SE3.Rz(np.pi).A
-    #     return np.linalg.inv(Hdh_to_urdf) @ H
-
-    # @staticmethod
-    # def _convert_dh_to_urdf_frame(H):
-    #     "from dh frame to our design task in urdf frame"
-    #     Hdh_to_urdf = SE3.Rz(np.pi).A
-    #     return Hdh_to_urdf @ H
-
 
 if __name__ == "__main__":
-    robot_real = RobotController()
+    # robot_real = RobotController()
     robot_kin = RobotUR5eKin()
-
-    robot_real_q = robot_real.get_actual_q()
-    print("Real robot joint angles (radians):\n", robot_real_q)
-
-    # robot_real.rtde_c.teachMode()
-
-    # robot_kin.setup_live_plot(frame_size=0.08)
-    # try:
-    #     while True:
-    #         new_q = robot_real.get_actual_q()
-    #         robot_kin.plot_link_transforms_live(new_q)
-    #         # In real use: sleep or wait for new sensor data
-    #         import time
-    #         time.sleep(0.1)
-    # except KeyboardInterrupt:
-    #     print("Exiting live plot loop.")
-    # robot_real.rtde_c.endTeachMode()
+    q = [0, -np.pi / 4, np.pi / 2, -np.pi / 4, -np.pi / 2, 0]
+    robot_kin.plot_link_transforms(q)
